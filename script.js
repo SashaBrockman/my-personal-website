d5b5ff1d154e4512d9302cb404a0410e46983a74
George <georgefbrockman@gmail.com>
2023-05-29 14:02:39 -0500

    add button, checks if file is uploaded on button press

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+const button = document.querySelector("button");
+const fileExists = document.querySelector("p");
+
+console.log(button.id);
+button.addEventListener("click", imageSpin);
+
+function imageSpin(){
+    const image = document.querySelector("#picture");
+    if(image.value.length > 0){
+        fileExists.textContent = "BEHOLD";
+    }
+}
+
+
+
